5e4211ed9eef78d0a6f0b2d2a9f5306c8abc7a99
yrayhan <yrayhan@purdue.edu>
2024-01-02 07:36:16 +0000

    push first

---
+++ build/CMakeFiles/erebus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/erebus.dir/src/erebus.cpp.o"
+  "CMakeFiles/erebus.dir/src/erebus.cpp.o.d"
+  "bin/erebus"
+  "bin/erebus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/erebus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

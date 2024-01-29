e2644844d5372561ebb8655716abe4696da1123c
yrayhan_bigdata <yrayhan@purdue.edu>
2024-01-29 00:17:16 -0500

    up qstamp features (result)

--- src/profiling/PerfCounters.hpp
+++ src/profiling/PerfCounters.hpp
@@ -10,6 +10,7 @@ struct PerfCounter
     double raw_counter_values[PERF_EVENT_CNT];
     int normalizationConstant = 1;
     erebus::storage::rtree::Rectangle rscan_query;
+    int result;
 };
 struct HWCounterStats{
     // TODO: This nees to be thread safe, hence use maybe concurrent_vector?

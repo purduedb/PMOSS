54e71a5f6f26568d30e14344f9603a807f89a749
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2024-09-27 16:43:41 -0400

    update wiki workloads

--- run_script.sh
+++ run_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ulimit -s unlimited
-for cfg in {40..79..5}; do
+
+for cfg in {1..79..5}; do
   /homes/yrayhan/works/erebus/build/bin/erebus $cfg
 done
+
+# for cfg in 51 52 53 57 58 71 72; do
+#   /homes/yrayhan/works/erebus/build/bin/erebus $cfg
+# done

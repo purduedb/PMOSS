ba1bca5707d4fe78f8ab41b6376f2862d9e371ce
yeasir <yeasirrayhan.prince@gmail.com>
2024-09-27 12:54:54 -0400

    clean code

--- run_script.sh
+++ run_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+ulimit -s unlimited
 
-for cfg in {2..59..2}; do
+# for cfg in {49..59..3}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg
+# done
+
+for cfg in 50 51 54 59 57; do
   /users/yrayhan/works/erebus/build/bin/erebus $cfg
 done

899bec03500bb109e97667949e1a4af7302c47ea
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2024-09-27 20:30:57 -0400

    integrate wiki ds

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ulimit -s unlimited
 
-# for cfg in {1..79..5}; do
-#   /homes/yrayhan/works/erebus/build/bin/erebus $cfg
-# done
-
-for cfg in 40 42 43 44 45 47 49; do
+for cfg in {1..79..5}; do
   /homes/yrayhan/works/erebus/build/bin/erebus $cfg
 done
+
+# for cfg in 40 42 43 44 45 47 49; do
+#   /homes/yrayhan/works/erebus/build/bin/erebus $cfg
+# done

2e699e45a5537e39a7bf38a8961c1d883c7cc704
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-02 15:30:09 -0400

    clean

--- src/threads/TPM.cc
+++ src/threads/TPM.cc
@@ -892,7 +892,7 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
       // for (auto gc = 0; gc < gm->nGridCells; gc++){
       //   valid_gcells.push_back(gc);   
       // }                
-      // if (valid_gcells.size() == 0) continue;  
+      if (valid_gcells.size() == 0) continue;  
       
       for(size_t qc1 = 0; qc1 < valid_gcells.size()-1; qc1++){
           int pCell = valid_gcells[qc1];
@@ -907,7 +907,7 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
         31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
         61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
-        91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+        91, 92, 93, 94, 95, 96, 97, 98, 99
       };
       
       // Push the query to the correct worker thread's job queue

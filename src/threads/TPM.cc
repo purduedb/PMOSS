90f0b167c207974930df5dfa4e56befb8a23ff42
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2024-10-06 10:26:44 -0400

    integrate workloads

--- src/threads/TPM.cc
+++ src/threads/TPM.cc
@@ -431,6 +431,7 @@ void TPManager::dump_ncoresweeper_threads(){
       dirName += "/kb_quad/" + std::to_string(key);
   #elif STORAGE == 2
       dirName += "/kb_b/" + std::to_string(key);
+      // dirName += "/kb_b_/" + std::to_string(key);
   #endif
   
   mkdir(dirName.c_str(), 0777);
@@ -914,7 +915,7 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
     else if (
       wl == SD_YCSB_WKLOADA || wl == SD_YCSB_WKLOADC || wl == SD_YCSB_WKLOADE ||
       wl == SD_YCSB_WKLOADF || wl == SD_YCSB_WKLOADE1 || wl == SD_YCSB_WKLOADH || 
-      wl == SD_YCSB_WKLOADI || 
+      wl == SD_YCSB_WKLOADI || wl == SD_YCSB_WKLOADA1 || wl == SD_YCSB_WKLOADH1 ||
       wl == WIKI_WKLOADA || wl == WIKI_WKLOADC || wl == WIKI_WKLOADE || wl == WIKI_WKLOADI ||
       wl == WIKI_WKLOADH || wl == WIKI_WKLOADA1 || wl == WIKI_WKLOADA2 || wl == WIKI_WKLOADA3 ||
       wl == OSM_WKLOADA || wl == OSM_WKLOADC || wl == OSM_WKLOADE || wl == OSM_WKLOADH
@@ -930,6 +931,10 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
 
       if (wl == SD_YCSB_WKLOADA){
         wl_config += "ycsb_workloada_" + to_string(router_cpuids[i]);
+        input.open(wl_config);
+      }
+      else if (wl == SD_YCSB_WKLOADA1){
+        wl_config += "ycsb_workloada1_" + to_string(router_cpuids[i]);
         input.open(wl_config);
       }
       else if (wl == SD_YCSB_WKLOADC){
@@ -950,6 +955,10 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
       }
       else if (wl == SD_YCSB_WKLOADH){
         wl_config += "ycsb_workloadh";
+        input.open(wl_config);
+      }
+      else if (wl == SD_YCSB_WKLOADH1){
+        wl_config += "ycsb_workloadh1";
         input.open(wl_config);
       }
       else if (wl == SD_YCSB_WKLOADI){
@@ -1178,7 +1187,7 @@ void TPManager::init_router_threads(int ds, int wl, double min_x, double max_x, 
     else if (
       wl == SD_YCSB_WKLOADA || wl == SD_YCSB_WKLOADC || wl == SD_YCSB_WKLOADE ||
       wl == SD_YCSB_WKLOADF || wl == SD_YCSB_WKLOADE1 || wl == SD_YCSB_WKLOADH || 
-      wl == SD_YCSB_WKLOADI || 
+      wl == SD_YCSB_WKLOADI || wl == SD_YCSB_WKLOADA1 || wl == SD_YCSB_WKLOADH1 ||
       wl == WIKI_WKLOADA || wl == WIKI_WKLOADC || wl == WIKI_WKLOADE || wl == WIKI_WKLOADI || 
       wl == WIKI_WKLOADH || wl == WIKI_WKLOADA1 || wl == WIKI_WKLOADA2 || wl == WIKI_WKLOADA3 ||
       wl == OSM_WKLOADA || wl == OSM_WKLOADC || wl == OSM_WKLOADE || wl == OSM_WKLOADH

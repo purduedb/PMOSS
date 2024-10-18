9b8f6903c6b53a1228fab1ce3e11fa8fdd03c316
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-18 13:20:24 -0400

    add 256 config

--- run_script.sh
+++ run_script.sh
@@ -2,17 +2,41 @@
 ulimit -s unlimited
 
 
-for cfg in {0..29..1}; do
+
+for cfg in {30..49..1}; do
   /users/yrayhan/works/erebus/build/bin/erebus $cfg 11
 done
-
-for cfg in {0..29..1}; do
+for cfg in {30..49..1}; do
   /users/yrayhan/works/erebus/build/bin/erebus $cfg 39
 done
-
-for cfg in {0..29..1}; do
+for cfg in {30..49..1}; do
   /users/yrayhan/works/erebus/build/bin/erebus $cfg 40
 done
+
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 12
+# done
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 16
+# done
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 32
+# done
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 41
+# done
+
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 34
+# done
+
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 35
+# done
+
+# for cfg in {30..49..1}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 36
+# done
 
 
 

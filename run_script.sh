978d2653389fb22be7b4b95c3026dba14fa6be81
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-21 10:00:52 -0600

    clean

--- run_script.sh
+++ run_script.sh
@@ -5,18 +5,38 @@ ulimit -s unlimited
 export PCM_NO_MSR=1
 export PCM_NMI_WATCHDOG=1
 
-for cfg in {1..59..3}; do
-  /users/yrayhan/works/erebus/build/bin/erebus $cfg 34
+
+# for wl in 12 16 32 41 34 35 36; do
+#   for cfg in 100 101 102 103; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
+# done
+
+# for wl in 11 39 40 13; do
+#   for cfg in 100 101 102 103; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
+# done
+
+for wl in 13; do
+  for cfg in 100 101 102 103; do
+    /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+  done
 done
 
-for cfg in {1..59..3}; do
-  /users/yrayhan/works/erebus/build/bin/erebus $cfg 35
-done
+# for cfg in {57..59..3}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 34
+# done
 
-for cfg in {1..59..3}; do
-  /users/yrayhan/works/erebus/build/bin/erebus $cfg 36
-done
+# for cfg in {1..59..3}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 35
+# done
 
+# for cfg in {1..59..3}; do
+#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 36
+# done
+
+# ---------------
 
 # for cfg in {31..59..1}; do
 #   /users/yrayhan/works/erebus/build/bin/erebus $cfg 11

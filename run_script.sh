a298d7a01a1c7bb2c35c615d92358b724ecc1a4f
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2024-10-20 22:27:20 -0400

    add sn-thread basline config

--- run_script.sh
+++ run_script.sh
@@ -4,20 +4,20 @@ ulimit -s unlimited
 export PCM_NO_MSR=1
 export PCM_NMI_WATCHDOG=1
 
-for cfg in {1..49..2}; do
-  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 12
+for cfg in {2..49..2}; do
+  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 11
 done
 
-for cfg in {1..49..2}; do
-  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 16
+for cfg in {2..49..2}; do
+  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 39
 done
 
-for cfg in {1..49..2}; do
-  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 32
+for cfg in {2..49..2}; do
+  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 40
 done
 
-for cfg in {1..49..2}; do
-  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 41
+for cfg in {2..49..2}; do
+  /homes/yrayhan/works/erebus/build/bin/erebus $cfg 13
 done
 
 

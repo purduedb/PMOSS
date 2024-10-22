a6166f002bb53111c76c4781f73c81e9e0da5e77
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-22 07:22:43 -0600

    push shell script

--- run_script.sh
+++ run_script.sh
@@ -18,38 +18,9 @@ export PCM_NMI_WATCHDOG=1
 #   done
 # done
 
-for wl in 13; do
-  for cfg in 100 101 102 103; do
+
+for wl in 11 13; do
+  for cfg in {1..59..4}; do
     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
   done
 done
-
-# for cfg in {57..59..3}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 34
-# done
-
-# for cfg in {1..59..3}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 35
-# done
-
-# for cfg in {1..59..3}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 36
-# done
-
-# ---------------
-
-# for cfg in {31..59..1}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 11
-# done
-
-# for cfg in {31..59..1}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 13
-# done
-
-# for cfg in {31..59..1}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 39
-# done
-
-# for cfg in {31..59..1}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 40
-# done

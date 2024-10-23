1e1c91e697faad7495afc9e3b07e0001575f3cad
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-23 10:12:49 -0400

    add pmoss configs

--- run_script.sh
+++ run_script.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 ulimit -s unlimited
 
+# Baselines:Run the read workloads
+# for wl in 12 16 32 41 34 35 36; do
+#   for cfg in 100 101 102 103; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
+# done
 
-for wl in 12 16 32 41 34 35 36; do
-  for cfg in 100 101 102 103; do
+# Baselines:Run the write workloads 
+# for wl in 11 39 40 13; do
+#   for cfg in 100 101 102 103; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
+# done
+
+
+# for wl in 12 32 41; do
+#   for cfg in 200; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
+# done
+
+for wl in 12; do
+  for cfg in 202; do
     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
   done
 done
-# 101 41
 
-# # for cfg in {1..59..2}; do
-# #   /users/yrayhan/works/erebus/build/bin/erebus $cfg 11
-# # done
-# for cfg in {27..59..2}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 13
-# done
-# for cfg in {1..59..2}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 39
-# done
-# for cfg in {1..59..2}; do
-#   /users/yrayhan/works/erebus/build/bin/erebus $cfg 40
+# for wl in 11 13; do
+#   for cfg in {2..69..3}; do
+#     /users/yrayhan/works/erebus/build/bin/erebus $cfg $wl
+#   done
 # done
 

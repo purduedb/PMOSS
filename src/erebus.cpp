c6e4536fe5bfceffef7b2e4f38716168c70c60b6
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-30 03:21:31 -0400

    fix osm dataset support

--- src/erebus.cpp
+++ src/erebus.cpp
@@ -364,9 +364,8 @@ void Erebus::register_threadpool(erebus::tp::TPManager *tp)
 int main(int argc, char* argv[])
 {	
 
-	int eval_pmoss = 1;
 	int cfgIdx = 1;
-	int ds = YCSB;
+	int ds = OSM_CELLIDS;
 	int wl = SD_YCSB_WKLOADX2;
 	int iam = BTREE;
 	
@@ -410,8 +409,9 @@ int main(int argc, char* argv[])
 		min_x = 1; max_x = 18446744073709551615; min_y = -1; max_y = -1; 
 	}
 	else if (ds == OSM_CELLIDS){
+		min_x = 0; max_x = 18446744073709551615; min_y = -1; max_y = -1;  //3400Mish points
 		// min_x = 33246697004540789; max_x = 13748549577969753901; min_y = -1; max_y = -1;  	//100M points
-		min_x = 33246697004540789; max_x = 5170332552548576529; min_y = -1; max_y = -1;  //200M points
+		// min_x = 33246697004540789; max_x = 5170332552548576529; min_y = -1; max_y = -1;  //200M points
 		// min_x = 33246697004540789; max_x = 13748551737189149045; min_y = -1; max_y = -1;  	//800M points
 		// min_x = 33246697004540789; max_x = 13748550930623082253; min_y = -1; max_y = -1;  	//200M points
 	}

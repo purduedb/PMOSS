78ad3ef9542eb5cd7e4b0ebe3aa046f6dc22a413
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-31 00:57:35 -0400

    clean learned

--- src/dataset/down_sample.py
+++ src/dataset/down_sample.py
@@ -10,14 +10,14 @@ def downsample(fn):
     d = np.fromfile("/users/yrayhan/works/erebus/src/dataset/" + fn + "_800M_uint64", dtype=np.uint64)[1:]
     nd = np.delete(d, np.arange(0, d.size, 4))
 
-    # with open("/scratch1/yrayhan/" + fn + "_600M_uint64", "wb") as f:
-    #     f.write(struct.pack("Q", len(nd)))
-    #     nd.tofile(f)
-
-    nd = d[::2]
-    with open("/users/yrayhan/works/erebus/src/dataset/" + fn + "_400M_uint64", "wb") as f:
+    with open("/users/yrayhan/works/erebus/src/dataset/" + fn + "_600M_uint64", "wb") as f:
         f.write(struct.pack("Q", len(nd)))
         nd.tofile(f)
+
+    # nd = d[::2]
+    # with open("/users/yrayhan/works/erebus/src/dataset/" + fn + "_400M_uint64", "wb") as f:
+    #     f.write(struct.pack("Q", len(nd)))
+    #     nd.tofile(f)
 
     # nd = d[::4]
     # with open("/users/yrayhan/works/erebus/src/dataset/" + fn + "_200M_uint64", "wb") as f:

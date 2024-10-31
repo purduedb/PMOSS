b0e5dd945830d37bee4e119caaaaf949030915d0
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-31 00:58:00 -0400

    clean

--- src/dataset/create.py
+++ src/dataset/create.py
@@ -5,34 +5,35 @@ files = [
 # "fb_200M_uint64",
 # "osm_cellids_100M_uint64"
 # "osm_cellids_200M_uint64"
-"osm_cellids_400M_uint64"
+"osm_cellids_600M_uint64"
 # "loade_zipf_int_100M.dat"
 ]
 
 # Initialize variables to store min and max values
 min_value = float('inf')
 max_value = float('-inf')
-file_name = "/users/yrayhan/works/erebus/src/dataset/" + files[0] 
+# file_name = "/users/yrayhan/works/erebus/src/dataset/" + files[0] 
 # Open and read the file
-with open(file_name, 'r') as file:
-    for line in file:
-        # Split the line by spaces
-        parts = line.split()
-        if len(parts) == 2 and parts[0] == "INSERT":
-            value = int(parts[1])  # Extract the second column value
-            # Update min and max values
-            min_value = min(min_value, value)
-            max_value = max(max_value, value)
+# with open(file_name, 'r') as file:
+#     for line in file:
+#         # Split the line by spaces
+#         parts = line.split()
+#         if len(parts) == 2 and parts[0] == "INSERT":
+#             value = int(parts[1])  # Extract the second column value
+#             # Update min and max values
+#             min_value = min(min_value, value)
+#             max_value = max(max_value, value)
 
 # Print the results
-print("Minimum value in the 2nd column:", min_value)
-print("Maximum value in the 2nd column:", max_value)
+# print("Minimum value in the 2nd column:", min_value)
+# print("Maximum value in the 2nd column:", max_value)
 
 for _ in files:
   fname = "/users/yrayhan/works/erebus/src/dataset/" + _  
   pts = np.fromfile(fname, dtype=np.uint64)
-  size = 400000000
+  size = 340000000
   print(pts.shape)
+  print(pts[0:30])
   print(min(pts[1:size+2]), max(pts[1:size+2]))
 exit(0)
 

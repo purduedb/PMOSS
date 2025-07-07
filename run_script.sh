839b29496047873923ca2c156c7cbb06bdbbd555
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2025-07-07 06:50:02 -0400

    Update workloads

--- run_script.sh
+++ run_script.sh
@@ -7,8 +7,24 @@ export PCM_NMI_WATCHDOG=1
 current_dir=$(pwd)
 exec="$current_dir/build/bin/erebus"
 
-for wl in 12; do
-  for cfg in {1..50..2}; do
+for wl in 11; do
+  for cfg in {1..30..3}; do
+    "$exec" $cfg $wl
+  done
+  for cfg in {30..80..3}; do
+    "$exec" $cfg $wl
+  done
+  for cfg in {80..100..3}; do
     "$exec" $cfg $wl
   done
 done
+
+
+# for wl in 12; do
+# `for cfg in {1..50..2}; do
+#     "$exec" $cfg $wl
+#   done
+#   for cfg in {50..100..3}; do
+#     "$exec" $cfg $wl
+#   done
+# done

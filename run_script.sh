4683e1cdcf00c1fefd3b529549fc92c333b552a9
Yeasir <yeasirrayhan.prince@gmail.com>
2025-07-08 21:29:45 +0000

    Clean

--- run_script.sh
+++ run_script.sh
@@ -8,9 +8,8 @@ current_dir=$(pwd)
 echo "Current directory: $current_dir"
 exec="$current_dir/build/bin/erebus"
 
-# AMD EPYC 7543 2S_8N [11, 12, 13, 44, 45]
-# 11
-for wl in 44 45 13; do
+# AMD EPYC 7543 2S_8N [11, 12, 13, 44, 45, 16]
+for wl in 11; do
   numactl --interleave=0,1,2,3,4,5,6,7 "$exec" 501 $wl
   for cfg in 500 502 506; do
     "$exec" $cfg $wl

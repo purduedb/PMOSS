7e8568d765b576c5f7932d8194b352faf6bbad6e
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2025-07-07 23:56:26 -0400

    m

--- run_script.sh
+++ run_script.sh
@@ -5,8 +5,8 @@ current_dir=$(pwd)
 exec="$current_dir/build/bin/erebus"
 
 
-for wl in 11; do
-  for cfg in {3..50..2}; do
+for wl in 12; do
+  for cfg in {1..50..3}; do
     "$exec" $cfg $wl
   done
 done

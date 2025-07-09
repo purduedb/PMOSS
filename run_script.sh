9e827a17e7496962cede920255c6d56dcb796500
yrayhan_bigdata <yeasirrayhan.prince@gmail.com>
2025-07-09 10:15:52 -0400

    Done with intel skx

--- run_script.sh
+++ run_script.sh
@@ -9,13 +9,21 @@ echo "Current directory: $current_dir"
 exec="$current_dir/build/bin/erebus"
 
 # AMD EPYC 7543 2S_8N [11, 12, 13, 44, 45, 16]
-for wl in 11; do
-  numactl --interleave=0,1,2,3,4,5,6,7 "$exec" 501 $wl
-  for cfg in 500 502 506; do
-    "$exec" $cfg $wl
-  done
-done
+# INTEL SKX [44 45 13 12 16 11]
+# for wl in ; do
+#   numactl --interleave=0,1,2,3,4,5,6,7 "$exec" 501 $wl
+#   for cfg in 500 502 506; do
+#     "$exec" $cfg $wl
+#   done
+# done
 
+# INTEL SB [44 45 11]
+# for wl in 12 16; do
+#   numactl --interleave=0,1,2,3 "$exec" 501 $wl
+#   for cfg in 500 502 506; do
+#     "$exec" $cfg $wl
+#   done
+# done
 
 # AMD EPYC 7543 2S_2N [11, 12, 13, 44, 45, 16]
 # for wl in 16; do
@@ -25,16 +33,4 @@ done
 #   done
 # done
 
-# Intel SKX
-# for wl in 11; do
-#   numactl --interleave=0,1,2,3,4,5,6,7 "$exec" 501 $wl
-#   for cfg in 500 502 506; do
-#     "$exec" $cfg $wl
-#   done
-# done
 
-# for wl in 11; do
-#   for cfg in 500; do
-#     "$exec" $cfg $wl
-#   done
-# done

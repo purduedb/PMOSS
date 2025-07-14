18bb511e928e9b6e01d95a6ac9bbe2d97402ef6e
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2025-07-14 10:46:23 -0400

    m

--- run_script.sh
+++ run_script.sh
@@ -6,31 +6,31 @@ exec="$current_dir/build/bin/erebus"
 
 # [11 12 44 45 16]
 # [27]
-for wl in 28; do
-  for cfg in 100 101 102; do
-    "$exec" $cfg $wl
-  done
-  for cfg in {1..30..4}; do
-    "$exec" $cfg $wl
-  done
-  for cfg in {30..40..4}; do
-    "$exec" $cfg $wl
-  done
-  for cfg in {40..50..4}; do
-    "$exec" $cfg $wl
-  done
-  for cfg in {50..59..4}; do
-    "$exec" $cfg $wl
-  done
-done
-
-# Learned Models
-# [11]
-# for wl in 13; do
-#   for cfg in 211; do
+# for wl in 28; do
+#   for cfg in 100 101 102; do
+#     "$exec" $cfg $wl
+#   done
+#   for cfg in {1..30..4}; do
+#     "$exec" $cfg $wl
+#   done
+#   for cfg in {30..40..4}; do
+#     "$exec" $cfg $wl
+#   done
+#   for cfg in {40..50..4}; do
+#     "$exec" $cfg $wl
+#   done
+#   for cfg in {50..59..4}; do
 #     "$exec" $cfg $wl
 #   done
 # done
+
+# Learned Models
+# [11]
+for wl in 28; do
+  for cfg in 207; do
+    "$exec" $cfg $wl
+  done
+done
 
 
 ##############################################################################################################

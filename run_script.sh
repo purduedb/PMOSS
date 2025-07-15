377cae882cdb3612c31796d6ca197f88ef72432c
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2025-07-14 22:31:08 -0400

    m

--- run_script.sh
+++ run_script.sh
@@ -3,10 +3,12 @@ ulimit -s unlimited
 
 current_dir=$(pwd)
 exec="$current_dir/build/bin/erebus"
-# [37, 40, 40, 7, 200],
-# [34, 41, 51, 26, 201],
+# 12 34 41 51 26 201
+# 11 [37 40 40 7 200],
+# 26 [100 48 54 506 206]
+# 27 [100 101 58 506 207]
 for wl in 12; do
-  for cfg in 34 41 51 26 201; do
+  for cfg in 34 41 51 26; do
     "$exec" $cfg $wl
   done
 done

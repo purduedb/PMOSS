1beb60487618cf5311f367b80fb0be61e047fa10
Yeasir <yeasirrayhan.prince@gmail.com>
2025-07-17 11:41:35 +0000

    clean

--- run_script.sh
+++ run_script.sh
@@ -7,31 +7,34 @@ exec="$current_dir/build/bin/erebus"
 
 # Fix the workload for baseline of AMD_2S_8N
 # [44 45 12 16 13 11]
-# for wl in 16; do
-#   for cfg in 100 101 102; do
-#     "$exec" $cfg $wl
-#   done
-#   for cfg in {2..30..3}; do
-#     "$exec" $cfg $wl
-#   done
-#   for cfg in {31..40..3}; do
-#     "$exec" $cfg $wl
-#   done
-#   for cfg in {41..50..3}; do
-#     "$exec" $cfg $wl
-#   done
-#   for cfg in {51..59..3}; do
-#     "$exec" $cfg $wl
-#   done
-# done
+for wl in 11; do
+  for cfg in 41 42 43 45 46 47 49; do
+    "$exec" $cfg $wl
+  done
+  # for cfg in 100 101 102; do
+  #   "$exec" $cfg $wl
+  # done
+  # for cfg in {2..30..3}; do
+  #   "$exec" $cfg $wl
+  # done
+  # for cfg in {30..40..4}; do
+  #   "$exec" $cfg $wl
+  # done
+  # for cfg in {40..50..4}; do
+  #   "$exec" $cfg $wl
+  # done
+  # for cfg in {50..59..4}; do
+  #   "$exec" $cfg $wl
+  # done
+done
 
 # Learned Models
 # [11 44 45 12 ]
-for wl in 16; do
-  for cfg in 404; do
-    "$exec" $cfg $wl
-  done
-done
+# for wl in 16; do
+#   for cfg in 404; do
+#     "$exec" $cfg $wl
+#   done
+# done
 ##############################################################################################################
 
 

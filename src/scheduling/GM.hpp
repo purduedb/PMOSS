1e3bb5826921b6a4a6c2c6b02715d2a8c6156085
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2025-10-17 22:05:29 -0400

    Run osm workload

--- src/scheduling/GM.hpp
+++ src/scheduling/GM.hpp
@@ -15,8 +15,8 @@ using std::ofstream;
 // -------------------------------------------------------------------------------------
 #define EVAL_PMOSS 1
 
-#define SINGLE_DIMENSION_KEY_LIMIT 1000000000 // 343901273, 200000000
-#define BTREE_INIT_LIMIT 1000000000
+#define SINGLE_DIMENSION_KEY_LIMIT 800000000 // 1000000000 (YCSB)
+#define BTREE_INIT_LIMIT 600000000  // 600000000
 #define LIMIT 1000                  // test btree workload
 
 #define MAX_GRID_CELL 256
